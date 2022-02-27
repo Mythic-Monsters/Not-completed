58c70331e83806afc6cfaaf607f0b4109f37c4d9
Mythic-Monsters <73722034+Mythic-Monsters@users.noreply.github.com>
2022-02-26 19:45:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+function StartClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/3DjKeNJGB/model.json", ModelReady)
+}
+
+function ModelReady()
+{
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results)
+{
+if (error) {
+    console.error(error)
+}
+else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random()*255) + 1;
+    random_number_g = Math.floor(Math.random()*255) + 1;
+    random_number_b = Math.floor(Math.random()*255) + 1;
+}}
+
+document.getElementById("result_label").innerHTML = "I can hear a" + (results[0].label);
+document.getElementById("result_confidence").innerHTML = "Accuracy is" + (results[0].confidence*100).toFixed(2)+"%";
+document.getElementById("result_label").style.color = "rgb(" + random_number_r + random_number_g + random_number_b+")";
+document.getElementById("result_confidence").style.color = "rgb(" + random_number_r + random_number_g + random_number_b+")";
+
+img = document.getElementById("alien1");
+img1 = document.getElementById("alien2");
+img2 = document.getElementById("alien3");
+img3 = document.getElementById("alien4");
+
+if (results[0].label == "Background Noise")
+{
+img.src = "alien-01.gif";
+img1.src = "alien-02.png";
+img2.src = "alien-03.png";
+img3.src = "alien-04.png";
+}
+
+else if (results[0].label == "marble")
+{
+img.src = "alien-01.png";
+img1.src = "alien-01.gif";
+img2.src = "alien-01.png";
+img3.src = "alien-01.png";
+}
+
+else if (results[0].label == "KBM")
+{
+img.src = "alien-01.png";
+img1.src = "alien-02.png";
+img2.src = "alien-03.gif";
+img3.src = "alien-04.png";
+}
+
+else {
+img.src = "alien-01.png";
+img1.src = "alien-02.png";
+img2.src = "alien-03.png";
+img3.src = "alien-04.gif";
+}
